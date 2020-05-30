8c61d5c03e7149e98fd8ed2150cc82d0f5dd5d5e
Sarah Dörr <sarahdr996@googlemail.com>
2020-05-30 18:27:40 +0200

    bug fixes.

--- train.py
+++ train.py
@@ -9,13 +9,18 @@ from Datasets import MoleDataset
 class CNNTrainer():
     def __init__(self, config):
         self._input_folder = config.get("input_folder", "data/training_sets/HAM10000")
+        self._output_model = config["output_model"]
         self._batchsize = config.get("batch_size", 16)
         self._epochs = config.get("epochs", 50)
         self._val_every_epoch = config.get("val_every_epoch", 5)
         learning_rate = config.get("learning_rate", 0.001)
         learning_rate_decay = config.get("learning_rate_decay", 0.997)
 
-        transform = transforms.Compose([transforms.Resize(224),
+        if not os.path.exists(os.path.dirname(self._output_model)):
+            os.makedirs(os.path.dirname(self._output_model))
+
+        transform = transforms.Compose([transforms.ToPILImage(),
+                                        transforms.Resize(224),
                                         transforms.ToTensor()])
 
         train_dataset = MoleDataset(input_folder=os.path.join(self._input_folder, "train"),
@@ -24,17 +29,18 @@ class CNNTrainer():
 
         val_dataset = MoleDataset(input_folder=os.path.join(self._input_folder, "validation"),
                                   csv_file=os.path.join(self._input_folder, "labels.csv"),
-                                  transforms=transforms)
+                                  transform=transform)
 
         self._train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=self._batchsize,
                                                          shuffle=True, num_workers=4)
         self._val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=self._batchsize,
                                                        shuffle=False, num_workers=4)
 
-        self._model = models.resnet18(pretrained=True)
+        self._model = models.mobilenet_v2(pretrained=True)
 
         self._optimizer = torch.optim.Adam(self._model.parameters(), lr=learning_rate)
         self._scheduler = torch.optim.lr_scheduler.ExponentialLR(self._optimizer, learning_rate_decay)
+        self._loss = torch.nn.CrossEntropyLoss()
 
         if torch.cuda.is_available():
             self._use_gpu = True
@@ -45,6 +51,7 @@ class CNNTrainer():
             self._model.cuda()
 
     def train(self):
+        best_val_loss = 1000000
         for epoch in range(self._epochs):
             start = time.time()
 
@@ -61,15 +68,15 @@ class CNNTrainer():
                 loss = self._loss(outputs, labels)
                 loss.backward()
                 self._optimizer.step()
-                self._scheduler.step()
 
                 running_loss += loss.item()
             train_loss = running_loss / i
-            print("Epoch: {}/{}\nLearning Rate: {}\nTraining loss: {:03.4f}".format(epoch, self._epochs,
-                                                                                    self._scheduler.get_last_lr(),
+            print("Epoch: {}/{}\nLearning Rate: {}\nTraining loss: {:03.4f}".format(epoch + 1, self._epochs,
+                                                                                    self._scheduler.get_lr()[0],
                                                                                     train_loss))
+            self._scheduler.step()
 
-            if epoch % self._val_every_epoch == 0:
+            if (epoch + 1) % self._val_every_epoch == 0:
                 # Validation run
                 running_loss = 0
                 self._model.eval()
@@ -83,18 +90,23 @@ class CNNTrainer():
 
                     running_loss += loss.item()
                 val_loss = running_loss / i
-
                 print("Validation loss: {:03.4f}".format(val_loss))
+
+                if val_loss < best_val_loss:
+                    print("\nSaving model.\n")
+                    torch.save(self._model.state_dict(), self._output_model)
+                    best_val_loss = val_loss
 
             print("Time: {:03.2f}\n\n".format(time.time() - start))
 
         example = torch.rand(1, 3, 224, 224)
         traced_script_module = torch.jit.trace(self._model, example)
-        traced_script_module.save("app/src/main/assets/model.pt")
+        traced_script_module.save("app/src/main/assets/model_mobilnetv2.pt")
 
 
 if __name__ == "__main__":
     config = {"input_folder": "data/training_sets/HAM10000",
+              "output_model": "models/mobilenet_v2_2020_05_30.pth",
               "batch_size": 16,
               "epochs": 10,
               "val_every_epoch": 2,
